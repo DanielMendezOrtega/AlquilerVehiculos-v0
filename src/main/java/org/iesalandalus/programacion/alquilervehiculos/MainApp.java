a78fc3750b6f22697bf6efe4fb6bdca852a71732
Daniel <linoalmeria@gmail.com>
2023-02-16 14:03:52 +0100

    Clase cliente Finalizada sin errores

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -1,13 +1,23 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
-import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
-import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
-import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.dominio.Cliente;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 		// Ánimo!!!!
+		
+		Cliente cliente1 = new Cliente("Paco", "75720662Q", "628628628");
+	
+		System.out.println(cliente1.getTelefono());
+		System.out.println(cliente1.getDni());
+		System.out.println(cliente1.getNombre());
+		
+		
+		
+		
+		
+		
 	}
 
 }
